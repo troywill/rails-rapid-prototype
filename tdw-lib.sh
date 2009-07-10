1cea1bcf49bb1b87db8910fced5205923897fad2
Troy Will <troydwill@gmail.com>
2009-07-10 12:15:52 -0700

    modified:   tdw-lib.sh

--- tdw-lib.sh
+++ tdw-lib.sh
@@ -2,8 +2,8 @@ PORT=3000
 EDITOR=emacs
 DEV_DIR=rails_dev
 SERVER_SCRIPT="rails-server.sh"
-SCRIPT_DIR=".."
-TOP_DIR="../.."
+SCRIPT_DIR="../.."
+TOP_DIR="../../.."
 
 function restart_server () {
     cd ${TOP_DIR} && ./${SERVER_SCRIPT} ${PORT}
