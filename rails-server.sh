b3f6bd3cb448be5dd82ab003de6b275dc1e39ca7
Troy Will <troydwill@gmail.com>
2010-09-06 13:44:07 -0700

    rails-server.sh: For reasons I don't understand I was getting ActionController::InvalidAuthenticityToken errors, and redirect errors when I started an application with mongrel_rails directly.

--- rails-server.sh
+++ rails-server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 PORT=$1
+PORT=3001
 mongrel_rails stop
 rm log/mongrel.pid
 
-echo "mongrel_rails start -p $PORT -d"
-mongrel_rails start -p $PORT -d
+COMMAND="ruby script/server -p $PORT -d"
+echo $COMMAND
+$COMMAND
